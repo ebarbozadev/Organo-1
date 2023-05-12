ad49aefd50f0133b46620712b28b8e5ad88331fd
Emanuel Marcos dos Santos Barboza <emanuel@tigrara.local>
2023-05-12 12:48:55 -0300

    Atualizando arquivos

--- src/App.js
+++ src/App.js
@@ -218,8 +218,8 @@ function App() {
 
   const [colaboradores, setColaboradores] = useState(inicial)
 
-  function deletarColaborador() {
-    console.log('Colaborador deletado!');
+  function deletarColaborador(id) {
+    setColaboradores(colaboradores.filter(colaborador => colaborador.id !== id))
   }
 
   function mudarCorDoTime(cor, id){
@@ -231,10 +231,18 @@ function App() {
     }))
   }
 
+  function cadastrarTime() {
+    setTimes([...times, {...novoTime, id: uuidv4}])
+  }
+
   return (
     <div>
       <Banner />
-      <Formulario times={times.map(time => time.nome)} aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])} />
+      <Formulario 
+        cadastrarTime={cadastrarTime}
+        times={times.map(time => time.nome)} 
+        aoCadastrar={colaborador => setColaboradores([...colaboradores, colaborador])}
+      />
       <section className="times">
         <h1>Minha organização</h1>
         {times.map((time, indice) =>
